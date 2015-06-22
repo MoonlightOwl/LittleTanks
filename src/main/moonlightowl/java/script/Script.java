daa92598e417afedefb4743ada3a3c6f22c354c3
MoonlightOwl <murky.owl@gmail.com>
2015-06-22 15:56:55 +0200

    And there it is: my simple logger

--- src/main/moonlightowl/java/script/Script.java
+++ src/main/moonlightowl/java/script/Script.java
@@ -1,7 +1,9 @@
 package main.moonlightowl.java.script;
 
+import main.moonlightowl.java.Logger;
 import main.moonlightowl.java.world.World;
 import org.luaj.vm2.Globals;
+import org.luaj.vm2.LuaError;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 import org.luaj.vm2.lib.jse.JsePlatform;
@@ -33,7 +35,15 @@ public class Script {
 
     public static void runInit(World world){
         if(canExecute()){
-            globals.get("init").invoke(new LuaValue[] { CoerceJavaToLua.coerce(world) });
+            try {
+                globals.get("init").invoke(new LuaValue[]{CoerceJavaToLua.coerce(world)});
+            } catch(LuaError e){
+                log(e.getMessage());
+            }
         }
+    }
+
+    public static void log(String message){
+        Logger.log("[Lua Script] " + message);
     }
 }
